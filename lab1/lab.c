3b1cd4526bf96117f601ad50dde46c6c275ac732
Rusu Dinu <dinu@codingshadows.com>
2023-03-03 10:41:26 +0200

    lab 1 example

--- lab1/lab.c
+++ lab1/lab.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+// Display the minimum of three float numbers, read with scanf
+
+float min(float x, float y){
+    if (x < y)
+        return x;
+    return y;
+}
+
+float minThree(float x, float y, float z){
+    return min(min(x,y),z);
+}
+
 int main() {
-    printf("Hello world");
+    printf("the min of 3, 4, 10 %.2f \n", minThree(3,4,10));
+    return 0;
 }
---
+++ lab1/lab.h
@@ -0,0 +1,8 @@
+
+#ifndef DSA_PUBLIC_LAB_H
+#define DSA_PUBLIC_LAB_H
+
+float minOfThree(float a, float b, float c);
+
+
+#endif //DSA_PUBLIC_LAB_H
